2e63aadeac92c5c69bbf99c857c6530a530b6527
Karol Cedro <karol-cedro@wp.pl>
2021-04-02 15:51:48 +0200

    Add initial files

---
+++ src/main/java/com/kcedro/laptimesboard/LapTimesBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kcedro.laptimesboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapTimesBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LapTimesBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcedro/laptimesboard/LapTimesBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcedro.laptimesboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapTimesBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
