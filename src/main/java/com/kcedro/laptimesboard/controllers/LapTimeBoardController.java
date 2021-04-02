c6f2e2c1c663fd0e09ab914e6701edd2880ed07d
Karol Cedro <karol-cedro@wp.pl>
2021-04-02 16:49:44 +0200

    Add lap time board controller

---
+++ src/main/java/com/kcedro/laptimesboard/controllers/LapTimeBoardController.java
@@ -0,0 +1,33 @@
+package com.kcedro.laptimesboard.controllers;
+
+import com.kcedro.laptimesboard.entities.LapTime;
+import com.kcedro.laptimesboard.services.LapTimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("laptimeboard")
+public class LapTimeBoardController {
+
+    private LapTimeService lapTimeService;
+
+    @Autowired
+    public LapTimeBoardController(LapTimeService lapTimeService) {
+        this.lapTimeService = lapTimeService;
+    }
+
+    @GetMapping("/list")
+    public String listOfLapTimes(Model model){
+
+        List<LapTime> lapTimes = lapTimeService.findAll();
+
+        model.addAttribute("laptimes",lapTimes);
+
+        return "laptimesboard/laptime-list";
+    }
+}
